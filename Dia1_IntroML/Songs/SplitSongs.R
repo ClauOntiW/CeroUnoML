012efedbfc0e2a3b74aae953646dd6c7952b510b
José Antonio García <jose.ramirez@cimat.mx>
2018-11-18 16:24:14 -0600

    idem

--- Dia1_IntroML/Songs/SplitSongs.R
+++ Dia1_IntroML/Songs/SplitSongs.R
@@ -1,4 +1,4 @@
-setwd("~/YearPredictionMSD.txt") #fijamos el directorio de trabajo
+setwd("C:/Users/fou-f/Desktop/Third/ComputoEstadistico/ProyectoFinal/YearPredictionMSD.txt") #fijamos el directorio de trabajo
 dir()
 t1 <- Sys.time()
 songs <- read.csv('YearPredictionMSD.txt', stringsAsFactors = FALSE, header = FALSE)
@@ -8,9 +8,11 @@ names(songs) <- c('y', paste0('X', 1:90))
 set.seed(0) #fijamos una semilla para reproducir la particion
 n <- dim(songs)[1]
 index <- sample(1:n, round(n/100)) #muestra de 1%
-train.index <- sample(index, round(length(index)/70))
 sample <- songs[index, ]
+train.index <- sample(1:dim(sample)[1], round(dim(sample)[1]*(.8)))
 train <- sample[train.index, ]
 test <- sample[-train.index, ]
-write.csv(train, 'SongsTrain.csv')
-write.csv(test, 'SongsTest.csv')
+write.csv(train, 'SongsTrain.csv', row.names = FALSE)
+write.csv(test, 'SongsTest.csv', row.names = FALSE)
+plot(table(train$y))
+plot(table(test$y))
